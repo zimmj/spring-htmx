c3217c0fde615ed620b1f892c31ce5fc8ebce335
zimmj <33086958+zimmj@users.noreply.github.com>
2024-02-01 20:43:14 +0100

    ~feat(Init): Initialise spring boot application
    Initialise a spring boot application with:
    
    - spring-web
    - spring-thymeleaf

---
+++ src/main/java/tech/zimmerli/springhtmx/SpringHtmxApplication.java
@@ -0,0 +1,13 @@
+package tech.zimmerli.springhtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHtmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/zimmerli/springhtmx/SpringHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zimmerli.springhtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
